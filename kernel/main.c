73b8bf65dc46c225cda9f26a0bdf130d3256a4a6
JackWang <jack4shihong@gmail.com>
2022-12-05 07:47:48 -0600

    will be reset

--- kernel/main.c
+++ kernel/main.c
@@ -23,15 +23,15 @@ int main(void){
     put_str("In kernel now, start happy c time!\n");
     init_all();
 
-    // 用户进程调用系统调用
+    // use function to simulate user program
     process_execute(u_prog_a, "user_prog_a");
     process_execute(u_prog_b, "user_prog_b");
 
-    // 开中断, 使得时钟中断可以运行, 从而能够开始运行线程调度
+    // enable interrupt, so that 9253 timer functions and schedule() start to schedule user program
     intr_enable();
     console_put_str("main_pid: 0x"), console_put_int(sys_getpid()), console_put_char('\n');
 
-    // 内核线程输出用户进程pid
+    // kernel thread
     thread_start("k_thread_a", 31, k_thread_a, "argA ");
     thread_start("k_thread_b", 31, k_thread_b, "argB ");
     while(1);
@@ -44,6 +44,12 @@ void k_thread_a(void *arg){
     while(1);
 }
 
+void u_prog_a(void){
+    char *name = "prog_a";
+    printf("I am %s, my pid: 0x%x%c", name, getpid(), '\n');
+    while (1);
+}
+
 void k_thread_b(void *arg){
     char* para = (char *)arg;
     console_put_str("thread_b_pid: 0x"), console_put_int(sys_getpid()), console_put_char('\n');
@@ -51,15 +57,9 @@ void k_thread_b(void *arg){
 }
 
 
-void u_prog_a(void){
-    char *name = "prog_a";
-    printf("I am %s, my pid :%b%c", name, getpid(), '\n');
-    while (1);
-}
-
 
 void u_prog_b(void){
     char *name = "prog_b";
-    printf("I am %s, my pid :%d%c", name, getpid(), '\n');
+    printf("I am %s, my pid: 0x%d%c", name, getpid(), '\n');
     while(1);
 }
