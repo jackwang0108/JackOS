b68979359a2804392ed1374ad77932c39ef6ff77
JackWang <jack4shihong@gmail.com>
2022-12-15 08:11:15 -0600

    fixed typo

--- kernel/debug.c
+++ kernel/debug.c
@@ -16,6 +16,6 @@ void panic_spin(char *filename, int line, const char* func, const char* conditio
     put_str("filename: "); put_str(filename); put_str("\n");
     put_str("line: 0x"); put_int(line); put_str("\n");
     put_str("function: "); put_str((char*)func); put_str("\n");
-    put_str("condistion: "); put_str((char*)condition); put_str("\n");
+    put_str("condition: "); put_str((char*)condition); put_str("\n");
     while (1);
 }
